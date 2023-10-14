6343bb43517a5aca840edfa0b2e071942626f3c4
isobel-ai <isobel_ibironke@icloud.com>
2023-10-14 21:55:59 +0100

    Set-up

---
+++ EventSoc/src/config/firebaseConfig.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAh-K4jPf_6ugoEmaY8mE_JltG5zpSguvE",
+  authDomain: "eventsoc-5bfa8.firebaseapp.com",
+  projectId: "eventsoc-5bfa8",
+  storageBucket: "eventsoc-5bfa8.appspot.com",
+  messagingSenderId: "536019665706",
+  appId: "1:536019665706:web:8f9d3887dcbc0104dd1453",
+  measurementId: "G-BLJQFZR42D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
