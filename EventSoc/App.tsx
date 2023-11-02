db9898d6b91c68865b7ae58a2622a6ddadab5891
isobel-ai <isobel_ibironke@icloud.com>
2023-11-02 13:06:48 +0000

    Wrap project in navigation container

--- EventSoc/App.tsx
+++ EventSoc/App.tsx
@@ -1,15 +1,18 @@
 import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
-      <Box
-        width="100%"
-        justifyContent="center"
-        alignItems="center">
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
+      <NavigationContainer>
+        <Box
+          width="100%"
+          justifyContent="center"
+          alignItems="center">
+          <Text>Open up App.js to start working on your app!</Text>
+        </Box>
+      </NavigationContainer>
     </GluestackUIProvider>
   );
 }
