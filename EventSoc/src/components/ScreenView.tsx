01d02db78a00e034352f1b8a3cf0c410d110a5b9
isobel-ai <isobel_ibironke@icloud.com>
2023-11-04 21:23:28 +0000

    Allow multiplle JSX elements in ScreenView

--- EventSoc/src/components/ScreenView.tsx
+++ EventSoc/src/components/ScreenView.tsx
@@ -1,9 +1,6 @@
 import { View } from "@gluestack-ui/themed";
+import { ReactNode } from "react";
 
-export default function ScreenView({
-  children
-}: {
-  children: React.JSX.Element;
-}) {
+export default function ScreenView({ children }: { children: ReactNode }) {
   return <View style={{ paddingTop: 20 }}>{children}</View>;
 }
