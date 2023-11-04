cce0a560ef2e92d1df99f995ff6c07c2ca22ebee
isobel-ai <isobel_ibironke@icloud.com>
2023-11-04 15:54:27 +0000

    set up main tab navigation

--- EventSoc/App.tsx
+++ EventSoc/App.tsx
@@ -1,17 +1,13 @@
-import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
 import { NavigationContainer } from "@react-navigation/native";
+import MainTabNavigator from "./src/navigation/MainTabNavigator";
 
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <NavigationContainer>
-        <Box
-          width="100%"
-          justifyContent="center"
-          alignItems="center">
-          <Text>Open up App.js to start working on your app!</Text>
-        </Box>
+        <MainTabNavigator />
       </NavigationContainer>
     </GluestackUIProvider>
   );
