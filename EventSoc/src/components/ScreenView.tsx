5379575508d69a84993ba46ce6ccc7f7fa3ed228
isobel-ai <isobel_ibironke@icloud.com>
2023-11-05 01:12:01 +0000

    use props interface

--- EventSoc/src/components/ScreenView.tsx
+++ EventSoc/src/components/ScreenView.tsx
@@ -1,6 +1,10 @@
 import { View } from "@gluestack-ui/themed";
 import { ReactNode } from "react";
 
-export default function ScreenView({ children }: { children: ReactNode }) {
-  return <View style={{ paddingTop: 20 }}>{children}</View>;
+interface Props {
+  children: ReactNode;
+}
+
+export default function ScreenView(props: Props) {
+  return <View style={{ paddingTop: 20 }}>{props.children}</View>;
 }
