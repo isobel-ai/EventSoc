ab27900b990d4b65ff25b1ba138dbffa90252e09
isobel-ai <isobel_ibironke@icloud.com>
2023-11-05 20:49:07 +0000

    getEventPicture function

--- EventSoc/src/services/eventsService.ts
+++ EventSoc/src/services/eventsService.ts
@@ -1,6 +1,7 @@
 import { getDocs } from "firebase/firestore";
-import { eventsCol } from "../config/firebaseConfig";
+import { eventPicturesRef, eventsCol } from "../config/firebaseConfig";
 import SocEvent from "../models/SocEvent";
+import { getDownloadURL, ref } from "firebase/storage";
 
 export function getManagedEvents(
   setManagedEvents: React.Dispatch<React.SetStateAction<SocEvent[]>>
@@ -13,4 +14,17 @@ export function getManagedEvents(
       setManagedEvents(eventList);
     })
     .catch((err) => console.log("Error: ", err));
+}
+
+export function getEventPicture(
+  picUrl: string,
+  setImgUrl: React.Dispatch<React.SetStateAction<string>>
+) {
+  getDownloadURL(ref(eventPicturesRef, picUrl))
+    .then((url) => {
+      setImgUrl(url);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
