717f049579a5d019b53564d674b2b2c0c6165267
isobel-ai <isobel_ibironke@icloud.com>
2023-12-21 03:22:28 +0000

    only retrieve user from db on the first request

--- EventSoc/src/services/usersService.tsx
+++ EventSoc/src/services/usersService.tsx
@@ -1,17 +1,28 @@
 import { getDoc, doc } from "firebase/firestore";
 import { auth, usersCol } from "../config/firebaseConfig";
 import { User, defaultUser } from "../models/User";
+import { isEqual } from "lodash";
 
-export function retrieveUser() {
-  const id = auth.currentUser?.uid;
-  return getDoc(doc(usersCol, id))
-    .then((userSnapshot) => {
-      return userSnapshot.exists()
-        ? ({ ...userSnapshot.data(), id: id } as User)
-        : defaultUser;
-    })
-    .catch((err) => {
-      console.log(err);
-      return defaultUser;
-    });
+const user: User = defaultUser();
+
+export async function retrieveUser() {
+  // Retrieve user if the function hasn't been called before
+  if (isEqual(user, defaultUser())) {
+    const id = auth.currentUser?.uid;
+    Object.assign(
+      user,
+      await getDoc(doc(usersCol, id))
+        .then((userSnapshot) => {
+          return userSnapshot.exists()
+            ? ({ ...userSnapshot.data(), id: id } as User)
+            : user;
+        })
+        .catch((err) => {
+          console.log(err);
+          return user;
+        })
+    );
+  }
+
+  return user;
 }
