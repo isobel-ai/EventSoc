9f508fc98e1abf72d86b92f1d48265e9012f3c9e
isobel-ai <isobel_ibironke@icloud.com>
2023-12-23 01:10:52 +0000

    add 2xl size

--- EventSoc/config/theme/Icon.ts
+++ EventSoc/config/theme/Icon.ts
@@ -1,62 +1,70 @@
-import { createStyle } from '@gluestack-style/react';
+import { createStyle } from "@gluestack-style/react";
 
 export const BaseIcon = createStyle({
-  color: '$backgroundLight800',
+  color: "$backgroundLight800",
   _dark: {
-    color: '$backgroundDark400',
+    color: "$backgroundDark400"
   },
   variants: {
     size: {
-      '2xs': {
-        h: '$3',
-        w: '$3',
+      "2xs": {
+        h: "$3",
+        w: "$3",
         props: {
           // @ts-ignore
-          size: 12,
-        },
+          size: 12
+        }
       },
-      'xs': {
-        h: '$3.5',
-        w: '$3.5',
+      xs: {
+        h: "$3.5",
+        w: "$3.5",
         props: {
           //@ts-ignore
-          size: 14,
-        },
+          size: 14
+        }
       },
-      'sm': {
-        h: '$4',
-        w: '$4',
+      sm: {
+        h: "$4",
+        w: "$4",
         props: {
           //@ts-ignore
-          size: 16,
-        },
+          size: 16
+        }
       },
-      'md': {
-        h: '$4.5',
-        w: '$4.5',
+      md: {
+        h: "$4.5",
+        w: "$4.5",
         props: {
           //@ts-ignore
-          size: 18,
-        },
+          size: 18
+        }
       },
-      'lg': {
-        h: '$5',
-        w: '$5',
+      lg: {
+        h: "$5",
+        w: "$5",
         props: {
           //@ts-ignore
-          size: 20,
-        },
+          size: 20
+        }
       },
-      'xl': {
-        h: '$6',
-        w: '$6',
+      xl: {
+        h: "$6",
+        w: "$6",
         props: {
           //@ts-ignore
-          size: 24,
-        },
+          size: 24
+        }
       },
-    },
-  },
+      "2xl": {
+        h: "$7",
+        w: "$7",
+        props: {
+          //@ts-ignore
+          size: 28
+        }
+      }
+    }
+  }
   // defaultProps: {
   //   size: 'md',
   // },
@@ -64,13 +72,13 @@ export const BaseIcon = createStyle({
 
 export const Icon = createStyle({
   props: {
-    size: 'md',
+    size: "md",
     //@ts-ignore
-    fill: 'none',
+    fill: "none"
   },
-  color: '$backgroundLight800',
+  color: "$backgroundLight800",
   _dark: {
     //@ts-ignore
-    color: '$backgroundDark400',
-  },
+    color: "$backgroundDark400"
+  }
 });
