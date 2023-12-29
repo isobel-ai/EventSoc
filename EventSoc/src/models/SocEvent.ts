364ea57f8dbb753f01ee93ba5044d5333b4a2de0
isobel-ai <isobel_ibironke@icloud.com>
2023-12-29 13:06:51 +0000

    retrieve soc event model

---
+++ EventSoc/src/models/SocEvent.ts
@@ -0,0 +1,7 @@
+import { RetrieveEvent } from "./Event";
+import { RetrieveSociety } from "./Society";
+
+export interface RetrieveSocEvent {
+  society: RetrieveSociety;
+  event: RetrieveEvent;
+}
