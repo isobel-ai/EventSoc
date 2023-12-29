2def80efe63c5e95b09f44f1ba1d60003f2a4d79
isobel-ai <isobel_ibironke@icloud.com>
2023-12-29 20:35:07 +0000

    searchable to search filter list

--- EventSoc/src/screens/Societies/SocietiesScreen.tsx
+++ EventSoc/src/screens/Societies/SocietiesScreen.tsx
@@ -28,7 +28,7 @@ import { isEqual } from "lodash";
 import { RetrieveSociety } from "../../models/Society";
 import { config } from "../../../config/gluestack-ui.config";
 import { retrieveEvents } from "../../services/eventsService";
-import SearchableList from "../../components/SearchableList";
+import SearchableList from "../../components/SearchFilterList";
 import EventListButton from "../../components/EventListButton";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
