aaa7a19957d72b6339ad7621497fcb9b82cb2f53
isobel-ai <isobel_ibironke@icloud.com>
2024-01-02 23:59:55 +0000

    put tag in seperate component

---
+++ EventSoc/src/components/Tag.tsx
@@ -0,0 +1,46 @@
+import { View, Text, Icon, CloseCircleIcon } from "@gluestack-ui/themed";
+import { config } from "../../config/gluestack-ui.config";
+import { TextStyle, TouchableOpacity, ViewStyle } from "react-native";
+
+interface Props {
+  label: string;
+  onCancel: () => void;
+}
+
+export default function Tag(props: Props) {
+  const containerStyle: ViewStyle = {
+    flexDirection: "row",
+    borderRadius: 20,
+    paddingVertical: 5,
+    paddingRight: 5,
+    paddingLeft: 10,
+    marginRight: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: config.tokens.colors.navigationDarkPink,
+    flexGrow: 1
+  };
+
+  const labelStyle: TextStyle = {
+    color: "#fff",
+    fontSize: config.tokens.fontSizes.sm
+  };
+
+  return (
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{props.label}</Text>
+      <TouchableOpacity
+        style={{ marginLeft: 15 }}
+        hitSlop={{ top: 14, bottom: 14, left: 14, right: 14 }}
+        onPress={props.onCancel}>
+        <Icon
+          as={CloseCircleIcon}
+          fill="$navigationDarkPink"
+          color="white"
+          w={21}
+          h={21}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
