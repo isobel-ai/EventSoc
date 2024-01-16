62ac3249a8906b3323d47b464526aaf6a8cd1e79
isobel-ai <isobel_ibironke@icloud.com>
2024-01-16 23:49:18 +0000

    (withdraw)EventSignUp function

--- EventSoc/src/services/userEventService.ts
+++ EventSoc/src/services/userEventService.ts
@@ -1,11 +1,17 @@
-import { runTransaction, doc, getDoc, arrayUnion } from "firebase/firestore";
+import {
+  runTransaction,
+  doc,
+  getDoc,
+  arrayUnion,
+  arrayRemove
+} from "firebase/firestore";
 import { db, eventsCol, usersCol } from "../config/firebaseConfig";
 import { EventData } from "../models/Event";
 
 /**
  * @returns whether the sign-up is successful
  */
-export function signUp(attendeeId: string, eventId: string) {
+export function eventSignUp(attendeeId: string, eventId: string) {
   return runTransaction(db, (transaction) => {
     const eventDoc = doc(eventsCol, eventId);
     const attendeeDoc = doc(usersCol, attendeeId);
@@ -31,5 +37,20 @@ export function signUp(attendeeId: string, eventId: string) {
     });
   }).catch(() => {
     throw Error("Unable to sign-up. Try again later.");
+  });
+}
+
+export function withdrawEventSignUp(attendeeId: string, eventId: string) {
+  return runTransaction(db, (transaction) => {
+    const eventDoc = doc(eventsCol, eventId);
+    const attendeeDoc = doc(usersCol, attendeeId);
+
+    transaction
+      .update(eventDoc, { attendeeIds: arrayRemove(attendeeId) })
+      .update(attendeeDoc, { eventAttendingIds: arrayRemove(eventId) });
+
+    return Promise.resolve();
+  }).catch(() => {
+    throw Error("Unable to withdraw sign-up. Try again later.");
   });
 }
