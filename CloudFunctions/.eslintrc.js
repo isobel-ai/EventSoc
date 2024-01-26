24293c21f2bc0392abf943d4e6212d87ba360059
isobel-ai <isobel_ibironke@icloud.com>
2024-01-26 01:09:49 +0000

    set-up cloud funcs

---
+++ CloudFunctions/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "import/no-unresolved": 0,
+    "no-duplicate-imports": "error",
+    "require-atomic-updates": "error",
+    "block-scoped-var": "error",
+    "camelcase": "error",
+    "curly": "error",
+    "default-param-last": ["error"],
+    "dot-notation": "error",
+    "eqeqeq": ["error", "smart"],
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/ban-ts-comment":"off"
+  },
+};
