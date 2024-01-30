68c750979997ba844f2b1d56433154eb2f71cd1d
isobel-ai <isobel_ibironke@icloud.com>
2024-01-30 01:32:42 +0000

    refactor reply screen and fix scrolling

--- EventSoc/src/screens/Event/ReplyScreen.tsx
+++ EventSoc/src/screens/Event/ReplyScreen.tsx
@@ -8,112 +8,157 @@ import {
   Button,
   ButtonText,
   Divider,
-  FlatList,
   Heading,
   Icon,
   Text,
-  VStack
+  VStack,
+  View
 } from "@gluestack-ui/themed";
 import { useAppContext } from "../../contexts/AppContext";
 import React, { useEffect, useRef, useState } from "react";
 import {
   retrieveCommentData,
-  retrieveComments,
   retrieveReplies,
   retrieveReplyAncestry
 } from "../../services/commentsService";
-import { Comment, CommentData } from "../../models/Comment";
+import { Comment } from "../../models/Comment";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { config } from "../../../config/gluestack-ui.config";
 import CommentButton from "../../components/CommentButton";
 import CommentInputModal from "../../components/CommentInputModal";
 import { useIsFocused } from "@react-navigation/native";
-import { SectionList } from "react-native";
+import { ScrollView, FlatList, Dimensions } from "react-native";
 
 type Props = StackScreenProps<EventStackParamList, "Reply">;
 
 export default function ReplyScreen(props: Props) {
   const { userId } = useAppContext();
 
-  const [commentAncestry, setCommentAncestry] = useState<(Comment | Error)[]>(
-    []
-  );
-  const [replies, setReplies] = useState<(Comment | Error)[]>([]);
+  const [commentAncestry, setCommentAncestry] = useState<Comment[]>([]);
+  const [comment, setComment] = useState<Comment>();
+  const [replies, setReplies] = useState<Comment[]>([]);
+
+  const [retrieveComAncErrMsg, setRetrieveComAncErrMsg] = useState<string>("");
+  const [retrieveCommentErrMsg, setRetrieveCommentErrMsg] =
+    useState<string>("");
+  const [retrieveRepliesErrMsg, setRetrieveRepliesErrMsg] =
+    useState<string>("");
+
+  const [comAndRepsHeight, setComAndRepsHeight] = useState<number>();
+  const scrollViewRef = useRef<ScrollView>(null);
 
   const [showPostCommentModal, setShowPostCommentModal] =
     useState<boolean>(false);
 
   const isFocused = useIsFocused();
 
-  const sectionListRef = useRef<SectionList>(null);
+  useEffect(() => {
+    if (!showPostCommentModal && isFocused) {
+      retrieveCommentData(props.route.params.commentId)
+        .then((newCommentData) =>
+          setComment({ id: props.route.params.commentId, data: newCommentData })
+        )
+        .then(() => setRetrieveCommentErrMsg(""))
+        .catch((err) => !comment && setRetrieveCommentErrMsg(err.message));
+
+      retrieveReplies(props.route.params.commentId)
+        .then((newReplies) => setReplies(newReplies))
+        .then(() => setRetrieveRepliesErrMsg(""))
+        .catch(
+          (err) => !replies.length && setRetrieveRepliesErrMsg(err.message)
+        );
+    }
+  }, [isFocused, showPostCommentModal]);
 
   useEffect(() => {
     isFocused &&
-      retrieveReplyAncestry(props.route.params.commentId).then((newAncestory) =>
-        setCommentAncestry(newAncestory.reverse())
+      retrieveReplyAncestry(props.route.params.commentId).then(
+        (newAncestory) => {
+          setCommentAncestry(newAncestory.ancestry.reverse());
+          setRetrieveComAncErrMsg(newAncestory.error?.message ?? "");
+        }
       );
   }, [isFocused]);
 
-  useEffect(() => {
-    !showPostCommentModal &&
-      commentAncestry.length &&
-      retrieveCommentData(props.route.params.commentId)
-        .then((newCommentData) => {
-          setCommentAncestry(
-            commentAncestry.splice(commentAncestry.length - 1, 1, {
-              id: props.route.params.commentId,
-              data: newCommentData
-            })
-          );
-        })
-        .catch();
-  }, [showPostCommentModal]);
-
-  useEffect(() => {
-    retrieveReplies(props.route.params.commentId)
-      .then((newReplies) => setReplies(newReplies))
-      .catch((err) => !replies.length && setReplies([Error(err)]));
-  }, [commentAncestry.at(-1)]);
-
-  // Fix when loading is added
-  useEffect(() => {
-    commentAncestry.length &&
-      sectionListRef.current?.scrollToLocation({
-        sectionIndex: 0,
-        itemIndex: commentAncestry.length - 1
-      });
-  }, [commentAncestry.length]);
-
   return (
     <ScreenView extraStyle={{ height: "107%" }}>
-      <SectionList
-        ref={sectionListRef}
-        sections={[
-          { title: "ancestors", data: commentAncestry },
-          { title: "replies", data: replies }
-        ]}
-        renderItem={({ item }) => {
-          const comment = item as Comment;
-          return (
-            <CommentButton
-              comment={comment}
-              eventOrganiserId={props.route.params.eventOrganiserId}
-              disableButton={comment.id === props.route.params.commentId}
+      <ScrollView
+        ref={scrollViewRef}
+        maintainVisibleContentPosition={{ minIndexForVisible: 1 }}
+        contentContainerStyle={{ gap: 10 }}>
+        {retrieveComAncErrMsg && (
+          <Alert
+            action="error"
+            variant="outline"
+            width="100%"
+            alignSelf="center">
+            <MaterialIcons
+              name="error-outline"
+              size={40}
+              color={config.tokens.colors.error}
+              style={{ paddingRight: 15 }}
             />
-          );
-        }}
-        onScrollToIndexFailed={() => console.log("fail")}
-        stickySectionHeadersEnabled={true}
-        renderSectionHeader={({ section: { title } }) =>
-          title === "replies" ? (
+            <AlertText>{retrieveComAncErrMsg}</AlertText>
+          </Alert>
+        )}
+        <FlatList
+          scrollEnabled={false}
+          contentContainerStyle={{ paddingTop: 10, gap: 10 }}
+          data={commentAncestry}
+          renderItem={({ item }) => (
+            <CommentButton
+              comment={item}
+              eventOrganiserId={props.route.params.eventOrganiserId}
+            />
+          )}
+        />
+        <FlatList
+          scrollEnabled={false}
+          contentContainerStyle={{ paddingBottom: 10, gap: 10 }}
+          data={replies}
+          renderItem={({ item }) => (
+            <CommentButton
+              comment={item as Comment}
+              eventOrganiserId={props.route.params.eventOrganiserId}
+            />
+          )}
+          ListHeaderComponent={
             <VStack
-              backgroundColor={config.tokens.colors.backgroundLight100}
               gap={5}
-              paddingBottom={10}
               alignContent="center">
               <Divider
                 alignSelf="center"
                 width="95%"
+                marginBottom={5}
+                bgColor={config.tokens.colors.eventButtonGray}
+              />
+              {!retrieveCommentErrMsg ? (
+                comment && (
+                  <CommentButton
+                    comment={comment}
+                    eventOrganiserId={props.route.params.eventOrganiserId}
+                    disableButton
+                  />
+                )
+              ) : (
+                <Alert
+                  action="error"
+                  variant="outline"
+                  width="100%"
+                  alignSelf="center">
+                  <MaterialIcons
+                    name="error-outline"
+                    size={40}
+                    color={config.tokens.colors.error}
+                    style={{ paddingRight: 15 }}
+                  />
+                  <AlertText>{retrieveCommentErrMsg}</AlertText>
+                </Alert>
+              )}
+              <Divider
+                alignSelf="center"
+                width="95%"
+                marginTop={5}
                 marginBottom={-5}
                 bgColor={config.tokens.colors.eventButtonGray}
               />
@@ -124,7 +169,8 @@ export default function ReplyScreen(props: Props) {
               </Heading>
               <Button
                 width="93%"
-                onPress={() => setShowPostCommentModal(true)}>
+                onPress={() => setShowPostCommentModal(true)}
+                isDisabled={Boolean(retrieveRepliesErrMsg)}>
                 <Icon
                   as={AddIcon}
                   marginRight={5}
@@ -141,13 +187,45 @@ export default function ReplyScreen(props: Props) {
                 authorId={userId}
               />
             </VStack>
-          ) : (
-            <></>
-          )
-        }
-        style={{ marginTop: 10 }}
-        contentContainerStyle={{ paddingBottom: 10, gap: 10 }}
-      />
+          }
+          ListEmptyComponent={
+            retrieveRepliesErrMsg ? (
+              <Alert
+                action="error"
+                variant="outline"
+                width="95%"
+                alignSelf="center">
+                <MaterialIcons
+                  name="error-outline"
+                  size={40}
+                  color={config.tokens.colors.error}
+                  style={{ paddingRight: 15 }}
+                />
+                <AlertText>{retrieveRepliesErrMsg}</AlertText>
+              </Alert>
+            ) : (
+              <Text
+                fontSize={"$lg"}
+                alignSelf="center">
+                No Replies
+              </Text>
+            )
+          }
+          onLayout={({
+            nativeEvent: {
+              layout: { height }
+            }
+          }) => setComAndRepsHeight(height)}
+        />
+        <View
+          height={
+            comAndRepsHeight &&
+            comAndRepsHeight < Dimensions.get("screen").height - 100
+              ? Dimensions.get("screen").height - 100 - comAndRepsHeight
+              : undefined
+          }
+        />
+      </ScrollView>
     </ScreenView>
   );
 }
